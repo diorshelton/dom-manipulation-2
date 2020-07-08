664e09f78f71685c7860cbbc0e5e25752be2a388
Dior.Shelton <dior.shelton@gmail.com>
2020-07-08 18:41:20 -0400

    add button to html file, jQuery script that inserts images when button is clicked, script that removes images when each is clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-  $
-}
+  $("button").click(function() {
+    $("ul.motivation").after('<img src="img/yougotthis.jpg">', '<img src="img/believe.jpg">');
+    $("img").click(function() {
+      $(this).remove("img");
+    });
+  });
+});
